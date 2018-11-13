c907638db67f9541377647c3d090270d1350ec48
Malcom <mm@hhpty.com>
2018-11-13 15:29:15 +0200

    combined functions and added chalk

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
 const jwt = require('jsonwebtoken');
 const config = require('config');
+const chalk = require('chalk');
 
-module.exports = (req, res, next) => {
-  const token = req.header('x-auth-token');
-  if (!token) return res.status(401).send('Access denied. No token provided.');
+function auth(req, res, next) {
+  const token = req.header('malcom-auth-token');
+  if (!token) {
+    console.log(chalk.red('Access denied, No token provided'));
+    return res.status(401).send('Access denied. No token provided.');
+  }
+
 
   try {
     const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
-    req.user = decoded; 
+    req.user = decoded;
+    console.log(chalk.green('Valid Token'));
     next();
-  }
-  catch (ex) {
+  } catch (ex) {
+    console.log(chalk.red('Invalid Token'));
     res.status(400).send('Invalid token.');
   }
+}
+
+
+function admin(req, res, next) {
+  // 401 Unauthorized
+  // 403 Forbidden 
+
+  if (!req.user.isAdmin) {
+    console.log(chalk.red('Access denied, PRIVILEGE escalation issues'));
+    return res.status(403).send('Access denied.');
+  }
+
+  console.log(chalk.green('Admin Access Granted'));
+  next();
+}
+
+module.exports = {
+  auth,
+  admin
 }
